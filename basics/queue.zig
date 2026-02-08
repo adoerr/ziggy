ecc18b20ef393a55c33644d7989b1aedfe819645
adoerr <0xad@gmx.net>
2026-02-08 20:46:56 +0100

    Init Queue
    Signed-off-by: adoerr <0xad@gmx.net>

--- basics/queue.zig
+++ basics/queue.zig
@@ -7,5 +7,16 @@ pub fn Queue(comptime Child: type) type {
             data: Child,
             next: ?*Node,
         };
+        gpa: std.mem.Allocator,
+        start: ?*Node,
+        end: ?*Node,
+
+        pub fn init(gpa: std.mem.Allocator) Self {
+            return Self{
+                .gpa = gpa,
+                .start = null,
+                .end = null,
+            };
+        }
     };
 }
