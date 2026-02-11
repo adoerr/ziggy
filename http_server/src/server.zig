d1b6e5dbf0e8ce6c4997da701264b09b26778982
adoerr <0xad@gmx.net>
2026-02-11 08:53:01 +0100

    Add init()
    Signed-off-by: adoerr <0xad@gmx.net>

--- http_server/src/server.zig
+++ http_server/src/server.zig
@@ -1,1 +1,21 @@
-pub const Server = struct {};
+const std = @import("std");
+const net = std.Io.net;
+
+pub const Server = struct {
+    host []const u8,
+    port u16,
+    addr: net.IpAddress,
+    io: std.Io,
+
+    pub fn init(io: std.Io) !Server {
+        const host: []const u8 = "127.0.0.1";
+        const port: u16 = 3490;
+        const addr = try net.Ip4Address.parse(hostt, port);
+
+        return .{.host = host, .port = port, .addr = addr, .io = io};
+    }
+
+    pub fn listen(self: Server) !net.Server {
+
+    }
+};
