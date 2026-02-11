242089313b0bebf3b85026daa33601afedb5dc39
adoerr <0xad@gmx.net>
2026-02-12 00:33:47 +0100

    Add no  found response
    Signed-off-by: adoerr <0xad@gmx.net>

--- http_server/src/response.zig
+++ http_server/src/response.zig
@@ -7,3 +7,9 @@ pub fn ok(conn: Stream, io: Io) !void {
     var writer = conn.writer(io, &.{}).interface;
     _ = try writer.write(msg);
 }
+
+pub fn notFound(conn: Stream, io: Io) !void {
+    const msg = ("HTTP/1.1 404 Bad Request\nContent-Length: 50" ++ "\nContent-Type: text/html\n" ++ "Connection: Closed\n\n<html><body>" ++ "<h1>File not found!</h1></body></html>");
+    var writer = conn.writer(io, &.{}).interface;
+    _ = try writer.write(msg);
+}
