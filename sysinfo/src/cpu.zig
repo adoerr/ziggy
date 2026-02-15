33e3ddd4d02d9d5ba5a6b8dc65b8d2ce7f3283cc
adoerr <0xad@gmx.net>
2026-02-15 21:22:10 +0100

    Start func `parseLine`
    Signed-off-by: adoerr <0xad@gmx.net>

--- sysinfo/src/cpu.zig
+++ sysinfo/src/cpu.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const mem = std.mem;
 
 pub const CPUError = error{
-    UnexpectedFormat,
+    InvalidFormat,
 };
 
 pub const Sample = struct {
@@ -10,6 +10,17 @@ pub const Sample = struct {
     total: u64,
 };
 
-pub inline fn parseLine(_: []const u8) CPUError!Sample {
+pub inline fn parseLine(line: []const u8) CPUError!Sample {
+    // remove everything up to the first numerical value
+    _, const after = mem.cutScalar(u8, line, ' ') orelse return CPUError.InvalidFormat;
+    const values = mem.trimStart(u8, after, " ");
+
+    std.debug.print("Values: '{s}'\n", .{values});
+
     return .{ .idle = 0, .total = 0 };
+}
+
+test "parseLine" {
+    const line = "cpu  218940 1325 115910 98319183 16321 0 3538 0 0 0";
+    _ = try parseLine(line);
 }
