43f0d66df4ec9e03e2467dd01cec897b2727c741
adoerr <0xad@gmx.net>
2026-02-21 00:25:43 +0100

    Mini cat
    Signed-off-by: adoerr <0xad@gmx.net>

--- cat/src/main.zig
+++ cat/src/main.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-    _ = init.gpa;
+    const path = "./assets/meow.txt";
+    const alloc = init.gpa;
+
+    const cwd = std.Io.Dir.cwd();
+    const text = try cwd.readFileAlloc(init.io, path, alloc, .limited(16 * 1014 * 1024));
+    defer alloc.free(text);
+
+    try std.Io.File.stdout().writeStreamingAll(init.io, text);
 }
