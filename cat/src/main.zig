acd7a5fadfad9c70b3574bd20462c7d589bfd834
adoerr <0xad@gmx.net>
2026-02-21 23:37:33 +0100

    Use buffers
    Signed-off-by: adoerr <0xad@gmx.net>

--- cat/src/main.zig
+++ cat/src/main.zig
@@ -1,12 +1,29 @@
 const std = @import("std");
+const Io = std.Io;
 
 pub fn main(init: std.process.Init) !void {
     const path = "./assets/meow.txt";
-    const alloc = init.gpa;
+
+    var out_buf: [1024]u8 = undefined;
+    var out_writer = Io.File.stdout().writer(init.io, &out_buf);
+    var stdout = &out_writer.interface;
+
+    var err_buf: [1024]u8 = undefined;
+    var err_writer = Io.File.stderr().writer(init.io, &err_buf);
+    var stderr = &err_writer.interface;
 
     const cwd = std.Io.Dir.cwd();
-    const text = try cwd.readFileAlloc(init.io, path, alloc, .limited(16 * 1014 * 1024));
-    defer alloc.free(text);
 
-    try std.Io.File.stdout().writeStreamingAll(init.io, text);
+    const file = cwd.openFile(init.io, path, .{}) catch |err| {
+        try stderr.print("failed to open: {s}\n", .{path});
+        return err;
+    };
+    defer file.close(init.io);
+
+    var file_buf: [1014]u8 = undefined;
+    var file_reader = file.reader(init.io, &file_buf);
+    var reader = &file_reader.interface;
+
+    _ = try reader.streamRemaining(stdout);
+    try stdout.flush();
 }
