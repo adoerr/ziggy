f14d53a32f985275dd819f9100ed074efe1cdd7d
adoerr <0xad@gmx.net>
2026-02-24 09:58:38 +0100

    Count words
    Signed-off-by: adoerr <0xad@gmx.net>

--- ladder/src/root.zig
+++ ladder/src/root.zig
@@ -1,18 +1,12 @@
-//! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
-const Io = std.Io;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
+pub fn countWords(text: []const u8) usize {
+    var count: usize = 0;
+    var word_iter = std.mem.tokenizeScalar(u8, text, '\n');
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+    while (word_iter.next()) |_| {
+        count += 1;
+    }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+    return count;
 }
