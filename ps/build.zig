28b7ce60076935fbc4e20b1ac5e583995f76a127
adoerr <0xad@gmx.net>
2026-02-26 09:32:13 +0100

    Rename module root file
    Signed-off-by: adoerr <0xad@gmx.net>

--- ps/build.zig
+++ ps/build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("ps", .{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/ps.zig"),
         .target = target,
     });
 
