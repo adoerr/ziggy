83d4c32148daa0242500f4b4a9e89128b6ca3d35
adoerr <0xad@gmx.net>
2026-02-26 11:41:15 +0100

    Start Interpreter
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ ps/src/Interpreter.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+/// Object Model - in Porstscript everything is an object.
+const ValueType = enum { integer, float, name, executable };
+
+pub const Interpreter = struct {};
--- ps/src/ps.zig
+++ ps/src/ps.zig
@@ -1,1 +1,2 @@
 pub const Lexer = @import("Lexer.zig").Lexer;
+pub const Interpreter = @import("Interpreter.zig").Interpreter;
