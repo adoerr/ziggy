7fe7e075a66dc68634b4ad366a145d5554b6bfef
adoerr <0xad@gmx.net>
2026-02-28 00:17:14 +0100

    Validate host name WIP
    Signed-off-by: adoerr <0xad@gmx.net>

--- hostname/src/Hostname.zig
+++ hostname/src/Hostname.zig
@@ -1,7 +1,28 @@
+const std = @import("std");
+const testing = std.testing;
+
 const HostName = @This();
+
+/// Max supported host name length
+pub const MAX_LEN = 255;
 
 pub const ValidateError = error{ NameTooLong, InvalidHostName };
 
-pub fn validate(_: []const u8) ValidateError!void {
-    unreachable;
+/// Validates a hostname according to [RFC 1123](https://www.rfc-editor.org/rfc/rfc1123)
+pub fn validate(bytes: []const u8) ValidateError!void {
+    if (bytes.len == 0) return error.InvalidHostName;
+    if (bytes[0] == '.') return error.InvalidHostName;
+
+    // A trailing do in the FQDN doesn't cound toward host name length
+    const end = if (bytes[bytes.len - 1] == '.') end: {
+        if (bytes.len == 1) return error.InvalidHostName;
+        break :end bytes.len - 1;
+    } else bytes.len;
+
+    if (end > MAX_LEN) return error.NameTooLong;
+}
+
+test {
+    try testing.expectError(error.InvalidHostName, validate(""));
+    try testing.expectError(error.InvalidHostName, validate(".example.com"));
 }
