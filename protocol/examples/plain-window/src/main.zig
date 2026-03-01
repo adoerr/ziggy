5321049dda34430d5a4218c411432258db12faea
adoerr <0xad@gmx.net>
2026-03-01 15:29:50 +0100

    Add plain window example
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ protocol/examples/plain-window/src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const wayland = @import("wayland");
+
+const Io = std.Io;
+
+pub fn main(init: std.process.Init) !void {
+    var out_buffer: [1024]u8 = undefined;
+    var out_file_writer: Io.File.Writer = .init(.stdout(), init.io, &out_buffer);
+    const stdout = &out_file_writer.interface;
+
+    const addr = try wayland.Address.initEndpoint(init, "wayland-0");
+
+    try stdout.print("Address: {}\n", .{addr});
+    try stdout.flush();
+}
