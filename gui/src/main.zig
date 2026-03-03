78442ad50715a5d035e840296b1f5f6184a82133
adoerr <0xad@gmx.net>
2026-03-03 17:14:21 +0100

    Bind to globals
    Signed-off-by: adoerr <0xad@gmx.net>

--- gui/src/main.zig
+++ gui/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+
 const gui = @import("gui");
 const wayland = @import("wayland");
 const wl_client = @import("wl_client");
@@ -8,10 +9,13 @@ const Io = std.Io;
 const log = std.log.scoped(.main);
 
 var conn: wayland.Connection = undefined;
-const disp: wl_client.Display = .display; // object ID 1 is always implicitly assigned to `wl_display`
-var registry: wl_client.Registry = undefined;
+const display: wl_client.Display = .display; // object ID 1 is always implicitly assigned to `wl_display`
+var registry: wl_client.Registry = .invalid;
+var compositor: wl_client.Compositor = .invalid;
+var shm: wl_client.Shm = .invalid;
+var wm_base: xdg_shell.WmBase = .invalid;
 
-const Event = wayland.Message(.{wl_client});
+const Event = wayland.Message(.{ wl_client, xdg_shell });
 
 pub fn main(init: std.process.Init) !void {
     const addr = try wayland.Address.default(init);
@@ -24,15 +28,26 @@ pub fn main(init: std.process.Init) !void {
 
     log.info("Connected to {f}", .{addr});
 
-    registry = try disp.getRegistry(&conn);
-    _ = try disp.sync(&conn);
+    registry = try display.getRegistry(&conn);
+    _ = try display.sync(&conn);
 
     while (conn.nextMessage(Event, .none)) |event| {
         switch (event) {
             .wl_registry => |r| switch (r) {
                 .global => |g| {
-                    const n = @field(g, "interface");
-                    log.debug("Interface: `{s}`", .{n});
+                    // interface name
+                    const iface = @field(g, "interface");
+
+                    log.debug("Interface: `{s}`", .{iface});
+
+                    // bind to globals
+                    if (std.mem.eql(u8, iface, wl_client.Compositor.interface)) {
+                        compositor = try registry.bind(&conn, wl_client.Compositor, .v1, g.name);
+                    } else if (std.mem.eql(u8, iface, wl_client.Shm.interface)) {
+                        shm = try registry.bind(&conn, wl_client.Shm, .v1, g.name);
+                    } else if (std.mem.eql(u8, iface, xdg_shell.WmBase.interface)) {
+                        wm_base = try registry.bind(&conn, xdg_shell.WmBase, .v1, g.name);
+                    }
                 },
                 .global_remove => {
                     // Ignore
@@ -47,4 +62,7 @@ pub fn main(init: std.process.Init) !void {
         log.debug("Error {}", .{err});
         return err;
     }
+
+    // Assert all globals are bound
+    std.debug.assert(compositor != .invalid and shm != .invalid and wm_base != .invalid);
 }
