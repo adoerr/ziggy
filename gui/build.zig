ef292f38507aca544c2a518a13c1e3bf0254d327
adoerr <0xad@gmx.net>
2026-03-03 15:12:02 +0100

    Improve debug support
    Signed-off-by: adoerr <0xad@gmx.net>

--- gui/build.zig
+++ gui/build.zig
@@ -13,6 +13,8 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "gui",
+        .use_llvm = true,
+        .use_lld = true,
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
