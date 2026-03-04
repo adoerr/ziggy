75e578ebcfb9f243d6d84aa99fe2269616c0a8cf
adoerr <0xad@gmx.net>
2026-03-05 00:47:13 +0100

    Create a surface
    Signed-off-by: adoerr <0xad@gmx.net>

--- gui/src/main.zig
+++ gui/src/main.zig
@@ -1,87 +1,14 @@
 const std = @import("std");
-
 const gui = @import("gui");
-const wayland = @import("wayland");
-const wl_client = @import("wl_client");
-const xdg_shell = @import("xdg_shell");
 
 const Io = std.Io;
 const log = std.log.scoped(.main);
 
-const display: wl_client.Display = .display; // object ID 1 is always implicitly assigned to `wl_display`
-var registry: wl_client.Registry = .invalid;
-var compositor: wl_client.Compositor = .invalid;
-var shm: wl_client.Shm = .invalid;
-var wm_base: xdg_shell.WmBase = .invalid;
-var surface: wl_client.Surface = .invalid;
-
-const Event = wayland.Message(.{ wl_client, xdg_shell });
-
 pub fn main(init: std.process.Init) !void {
     var state = gui.State{};
 
-    const addr = try wayland.Address.default(init);
+    try gui.setup(init, &state);
+    defer gui.teardown();
 
-    state.connection = wayland.Connection.init(init.io, init.gpa, addr) catch |err| {
-        log.err("Failed to connect to {f}: {t}", .{ addr, err });
-        return err;
-    };
-    defer state.connection.deinit();
-
-    log.info("Connected to {f}", .{addr});
-
-    registry = try display.getRegistry(&state.connection);
-    _ = try display.sync(&state.connection);
-
-    while (state.connection.nextMessage(Event, .none)) |event| {
-        switch (event) {
-            .wl_registry => |r| switch (r) {
-                .global => |g| {
-                    // interface name
-                    const iface = @field(g, "interface");
-
-                    log.debug("Interface `{s}` available", .{iface});
-
-                    // bind to globals `wl_compositor`, `wl_shm` and `xdg_wm_base`
-                    if (std.mem.eql(u8, iface, wl_client.Compositor.interface)) {
-                        compositor = try registry.bind(&state.connection, wl_client.Compositor, .v1, g.name);
-                    } else if (std.mem.eql(u8, iface, wl_client.Shm.interface)) {
-                        shm = try registry.bind(&state.connection, wl_client.Shm, .v1, g.name);
-                    } else if (std.mem.eql(u8, iface, xdg_shell.WmBase.interface)) {
-                        wm_base = try registry.bind(&state.connection, xdg_shell.WmBase, .v1, g.name);
-                    }
-                },
-                .global_remove => {
-                    // Ignore
-                },
-            },
-            .wl_callback => {
-                break; // all globals received
-            },
-            else => log.err("Unexpected event: {}", .{event}),
-        }
-    } else |err| {
-        log.debug("Error {}", .{err});
-        return err;
-    }
-
-    // Assert all globals needed are bound
-    std.debug.assert(compositor != .invalid and shm != .invalid and wm_base != .invalid);
-
-    surface = try compositor.createSurface(&state.connection);
-    const xdg_surface = try wm_base.getXdgSurface(&state.connection, surface);
-    _ = try xdg_surface.getToplevel(&state.connection);
-    try surface.commit(&state.connection);
-
-    // Main loop
-    while (state.connection.nextMessage(Event, .none)) |event| switch (event) {
-        .xdg_wm_base => |ev| {
-            log.debug("xdg_wm_base event: {}", .{ev});
-            try wm_base.pong(&state.connection, ev.ping.serial);
-        },
-        else => log.info("Unexpected event: {}", .{event}),
-    } else |err| {
-        log.debug("Error {}", .{err});
-        return err;
-    }
+    try gui.createSurface(&state);
 }
--- gui/src/root.zig
+++ gui/src/root.zig
@@ -1,11 +1,18 @@
 const std = @import("std");
+
 const wayland = @import("wayland");
 const wl_client = @import("wl_client");
 const xdg_shell = @import("xdg_shell");
 
+const log = std.log.scoped(.gui);
+
+const display: wl_client.Display = .display; // `wl_display` has aways the special reserved id of `1`
+var connection: wayland.Connection = undefined;
+var registry: wl_client.Registry = .invalid;
+
+const Event = wayland.Message(.{ wl_client, xdg_shell });
+
 pub const State = struct {
-    connection: wayland.Connection = undefined,
-    registry: wl_client.Registry = .invalid,
     compositor: wl_client.Compositor = .invalid,
     shm: wl_client.Shm = .invalid,
     wm_base: xdg_shell.WmBase = .invalid,
@@ -13,6 +20,79 @@ pub const State = struct {
     buffer: wl_client.Buffer = .invalid,
     shared_memory: []align(4096) u8 = undefined,
 };
+
+pub fn setup(init: std.process.Init, state: *State) !void {
+    const addr = try wayland.Address.default(init);
+    connection = wayland.Connection.init(init.io, init.gpa, addr) catch |err| {
+        std.log.err("Failed to connect to {f}: {t}", .{ addr, err });
+        return err;
+    };
+
+    std.log.info("Connected to {f}", .{addr});
+
+    registry = try display.getRegistry(&connection);
+
+    // Sync display to know when all registry globals have benn received
+    _ = try display.sync(&connection);
+
+    while (connection.nextMessage(Event, .none)) |event| {
+        switch (event) {
+            .wl_registry => |r| switch (r) {
+                .global => |g| {
+                    // interface name
+                    const iface = @field(g, "interface");
+
+                    log.debug("Interface `{s}` available", .{iface});
+
+                    // bind to globals `wl_compositor`, `wl_shm` and `xdg_wm_base`
+                    if (std.mem.eql(u8, iface, wl_client.Compositor.interface)) {
+                        state.compositor = try registry.bind(&connection, wl_client.Compositor, .v1, g.name);
+                    } else if (std.mem.eql(u8, iface, wl_client.Shm.interface)) {
+                        state.shm = try registry.bind(&connection, wl_client.Shm, .v1, g.name);
+                    } else if (std.mem.eql(u8, iface, xdg_shell.WmBase.interface)) {
+                        state.wm_base = try registry.bind(&connection, xdg_shell.WmBase, .v1, g.name);
+                    }
+                },
+                .global_remove => {
+                    // Ignore
+                },
+            },
+            .wl_callback => {
+                break; // all globals received
+            },
+            else => log.err("Unexpected event: {}", .{event}),
+        }
+    } else |err| {
+        log.debug("Error {}", .{err});
+        return err;
+    }
+
+    // Assert all globals needed are bound
+    std.debug.assert(state.compositor != .invalid and state.shm != .invalid and state.wm_base != .invalid);
+}
+
+pub fn teardown() void {
+    defer connection.deinit();
+}
+
+pub fn createSurface(state: *State) !void {
+    state.surface = try state.compositor.createSurface(&connection);
+    const xdg_surface = try state.wm_base.getXdgSurface(&connection, state.surface);
+    _ = try xdg_surface.getToplevel(&connection);
+    try state.surface.commit(&connection);
+
+    // Main loop
+    while (connection.nextMessage(Event, .none)) |event| switch (event) {
+        .xdg_wm_base => |ev| {
+            log.debug("xdg_wm_base event: {}", .{ev});
+            try state.wm_base.pong(&connection, ev.ping.serial);
+        },
+        else => log.info("Unexpected event: {}", .{event}),
+    } else |err| {
+        log.debug("Error {}", .{err});
+        return err;
+    }
+}
 
 pub fn allocBuffer(state: *State, width: comptime_int, height: comptime_int) !void {
     const stride = width * 4;
@@ -24,10 +104,10 @@ pub fn allocBuffer(state: *State, width: comptime_int, height: comptime_int) !vo
     // Fill the buffer with white pixels
     @memset(state.shared_memory, 255);
 
-    const pool = try state.shm.createPool(&state.connection, fd, size);
+    const pool = try state.shm.createPool(&connection, fd, size);
     defer pool.destroy(&state.connection) catch {};
 
-    state.buffer = try pool.createBuffer(&state.connection, 0, width, height, stride, .argb8888);
+    state.buffer = try pool.createBuffer(&connection, 0, width, height, stride, .argb8888);
 }
 
 /// Create a new shared memory file truncated to `size` bytes.
