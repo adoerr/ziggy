412ef8b4c6c8a8899f96416374360a29a43a759a
adoerr <0xad@gmx.net>
2026-03-19 08:36:22 +0100

    Server init
    Signed-off-by: adoerr <0xad@gmx.net>

--- protocol/src/Server.zig
+++ protocol/src/Server.zig
@@ -6,11 +6,46 @@ const Connection = @import("Connection.zig");
 
 const Server = @This();
 
-const map_displays = 10;
+// Each Wayland client has an associated display structure. For each display a lock file
+// is crated. Hence this constant basically limits the number of Wayland clients a server
+// can deal with.
+const max_displays = 10;
 
 inner: std.Io.net.Server,
 lock: std.Io.File,
 path: [std.Io.net.UnixAddress.max_len:0]u8,
+
+pub const InitError = LockDisplayError || std.Io.Dir.OpenError || std.Io.net.UnixAddress.ListenError ||
+    error{ NoXdgRuntimeDir, NoDisplaysAvailable, NameTooLong, NoSpaceLeft };
+
+pub fn init(env: std.process.Init, io: std.Io) InitError!Server {
+    const xdg_runtime_dir_path = env.environ_map.get("XDG_RUNTIME_DIR") orelse return error.NoXdgRuntimeDir;
+    const xdg_runtime_dir = try std.Io.Dir.openDirAbsolute(io, xdg_runtime_dir_path, .{});
+    defer xdg_runtime_dir.close(io);
+
+    var endpoint_buf: [12]u8 = undefined;
+    var endpoint: []const u8 = undefined;
+
+    const lock: std.Io.File = for (0..max_displays) |display| {
+        endpoint = std.fmt.bufPrint(&endpoint_buf, "wayland-{}", .{display}) catch unreachable;
+        break lockDisplay(io, xdg_runtime_dir, endpoint) catch |err| {
+            if (err == error.LockFailed) continue;
+            return err;
+        };
+    } else return error.NoDisplaysAvailable;
+    errdefer lock.close(io);
+
+    var path_buf: [std.Io.net.UnixAddress.max_len]u8 = undefined;
+    const path = try std.fmt.bufPrint(&path_buf, "{s}/{s}", .{ xdg_runtime_dir_path, endpoint });
+    const addr = try std.Io.net.UnixAddress.init(path);
+
+    const server = try addr.listen(io, .{});
+
+    var self = Server{ .inner = server, .lock = lock, .path = @splat(0) };
+    @memcpy(self.path[0..path.len], path);
+
+    return self;
+}
 
 const LockDisplayError = std.Io.File.OpenError || std.Io.File.LockError || std.Io.File.StatError || error{LockFailed};
 
